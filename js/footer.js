5d92d67662fe5f252d4effc4f5df302b871eb32e
CassyB08 <cassyb145@gmail.com>
2026-05-16 21:58:50 -0500

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -6,8 +6,8 @@ class FooterComponent extends HTMLElement {
         <div class="container footer-inner">
           <div>&copy; ${year} Cassie Sanders</div>
           <div>
-            <a href="mailto:your-email@example.com">Email</a> &nbsp;|&nbsp;
-            <a href="https://www.linkedin.com/in/your-profile" target="_blank">LinkedIn</a>
+            <a href="mailto:cassyb145@gmail.com">Email</a> &nbsp;|&nbsp;
+            <a href="https://www.linkedin.com/in/cassandra-sanders/" target="_blank">LinkedIn</a>
           </div>
         </div>
       </footer>
