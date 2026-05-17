d2b854f8850059276d3196967fe6caf3328cb561
CassyB08 <cassyb145@gmail.com>
2026-05-16 22:49:30 -0500

    Update header.js

--- js/header.js
+++ js/header.js
@@ -4,7 +4,7 @@ class HeaderComponent extends HTMLElement {
       <header id="masthead">
         <div class="container">
           <nav class="main-nav">
-            <a id="logo" href="/">
+            <a id="logo" href="https://cassyb08.github.io/cassiesanders/">
   <img src="favicon.ico" alt="Cassie Sanders logo icon" class="logo-icon">Cassie Sanders</a>
             <ul class="navbar-menu">
               <li><a class="nav-link" href="#top-work">Work</a></li>
