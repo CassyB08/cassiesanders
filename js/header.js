f464bac77d6ae26477eab0c9d41672e0bde4b1a6
CassyB08 <cassyb145@gmail.com>
2026-05-16 22:11:25 -0500

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,37 +1,20 @@
-class Header extends HTMLElement {
-    constructor() {
-      super();
-    }
-
-    connectedCallback() {
-        this.innerHTML = `
-        <header id="masthead" class="site-header">
+class HeaderComponent extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <header id="masthead">
         <div class="container">
-          <nav class="main-nav navbar navbar-expand-lg">
-                <a class="navbar-brand" id="logo" href="/">Cassie Sanders</a>
-                <button class="navbar-toggler navbar-light" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarText">
-                  <ul class="navbar-nav mr-auto">
-                    <li class="active nav-item">
-                      <a href="/portfolio.html" class="nav-link">Portfolio</a><span class="sr-only">(current)</span>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/about.html" class="nav-link">About</a>
-                    </li>
-                    <li class="nav-item">
-                       <a href="/contact.html" class="nav-link">Contact</a>
-                    </li>
-                  </ul>
-                
-                </div>
-              </nav>
-            </div>
-        </header>
-        `;
-      }
-    }
+          <nav class="main-nav">
+            <a id="logo" href="/">Cassie Sanders</a>
+            <ul class="navbar-menu">
+              <li><a class="nav-link" href="#top-work">Work</a></li>
+              <li><a class="nav-link" href="#about-me">About</a></li>
+              <li><a class="nav-link" href="/portfolio.html" target="_blank">Portfolio</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    `;
+  }
+}
 
-
-  customElements.define('header-component', Header
+customElements.define('header-component', HeaderComponent);
