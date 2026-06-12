fb6f0c09391e68c9e7ed46c824e557c79c00abc0
CassyB08 <cassyb145@gmail.com>
2026-06-11 22:56:42 -0500

    Change header link to local index.html
    Updated header link to point to index.html instead of external URL.

--- js/header.js
+++ js/header.js
@@ -1,11 +1,11 @@
 class HeaderComponent extends HTMLElement {
   connectedCallback() {
-    this.style.display = "contents"; // keeps layout clean
+    this.style.display = "contents";
 
     this.innerHTML = `
       <header class="global-header py-3">
         <div class="container d-flex align-items-center">
-          <a href="https://cassyb08.github.io/cassiesanders/" class="brand text-decoration-none">
+          <a href="/index.html" class="brand text-decoration-none">
             <div class="brand-logo">CS</div>
             <span class="brand-name">Cassie Sanders</span>
           </a>
