9fc6dcb8f4ce76387aa81c10eba5e0d68be79a75
CassyB08 <cassyb145@gmail.com>
2026-06-12 12:37:25 -0500

    Refactor HeaderComponent with updated logo and link

--- js/header.js
+++ js/header.js
@@ -3,15 +3,14 @@ class HeaderComponent extends HTMLElement {
     this.style.display = "contents";
 
     this.innerHTML = `
-      <header class="global-header py-3">
+      <header class="global-header">
         <div class="container d-flex align-items-center">
-         <a class="brand text-decoration-none d-flex align-items-center">
-  <div class="brand-logo mr-2">
-    <img src="images/favicon.png.ico" alt="cassie sanders logo" />
-  </div>
-  <span class="brand-name">Cassie Sanders</span>
-</a>
-
+          <a href="https://cassyb08.github.io/cassiesanders/" class="brand d-flex align-items-center text-decoration-none">
+            <div class="brand-logo mr-2">
+              <img class="full-img" src="images/logo.png" alt="Cassie Sanders logo" />
+            </div>
+            <span class="brand-name">Cassie Sanders</span>
+          </a>
         </div>
       </header>
     `;
