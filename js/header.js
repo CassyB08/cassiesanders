d0644d790a5b11c8f182ce3cf71b018359394f61
CassyB08 <cassyb145@gmail.com>
2026-06-12 12:55:05 -0500

    Update header.js

--- js/header.js
+++ js/header.js
@@ -7,7 +7,7 @@ class HeaderComponent extends HTMLElement {
         <div class="container d-flex align-items-center">
           <a href="https://cassyb08.github.io/cassiesanders/" class="brand d-flex align-items-center text-decoration-none">
             <div class="brand-logo mr-2">
-              <img class="full-img" src="images/logo.png" alt="Cassie Sanders logo" />
+              <img class="full-img" src="images/favicon.png.ico" alt="Cassie Sanders logo" />
             </div>
             <span class="brand-name">Cassie Sanders</span>
           </a>
